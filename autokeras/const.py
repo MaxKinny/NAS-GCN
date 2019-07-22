77bde7442a19d0ee2b99383140eeedd5f55d23e4
maxgundam <maxgundam@hotmail.com>
2019-07-23 00:11:21 +0800

    fix the repository

--- autokeras/const.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-
-class Constant(object):
-    # Text
-    VOCABULARY_SIZE = 20000
--- mnist.py
+++ mnist.py
@@ -1,11 +1,19 @@
 from keras.datasets import mnist
-from autokeras import ImageClassifier
+from autokeras_NAS import ImageClassifier
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
+    # Strange!!
+    # nest line actually calls:
+    # NetworkModule class from:
+    # anaconda3/envs/auto-keras/lib/python3.6/site-packages/autokeras/net_module.py
+    # cos code: "from autokeras import ImageClassifier"
+    # when you execute it, it will call autokeras from anaconda env's site-packages
+    # however the grammar checker will track it from the local package!!!
+    # they are different!!!!!
     clf = ImageClassifier(verbose=True, augment=False)
     clf.fit(x_train, y_train, time_limit=30 * 6000)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
